3e1d788637708c642df03d01c0216a1c8e5f1188
Alejandro Dustet <aledustet@gmail.com>
2019-05-21 21:53:17 -0400

    Add test with enzyme

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,6 +5,12 @@ import 'jest-dom/extend-expect'
 import { MockedProvider } from 'react-apollo/test-utils';
 import { errorMocks, countriesWithCubaQueryMock } from './query.mocks';
 
+import Enzyme, { mount } from 'enzyme';
+import wait from 'waait';
+import { CountryDetails } from './Country';
+import Adapter from 'enzyme-adapter-react-16';
+Enzyme.configure({ adapter: new Adapter() })
+
 // automatically unmount and cleanup DOM after the test is finished.
 afterEach(cleanup)
 
@@ -28,7 +34,6 @@ it('renders the error message when it fails', async () => {
   expect(errorTextElement).toHaveTextContent(errorMessage);
 })
 
-
 it('renders the country and the continent name when the query returns a response', async () => {
   const { getByText } = render(
     <MockedProvider mocks={countriesWithCubaQueryMock} addTypename={false}>
@@ -39,4 +44,19 @@ it('renders the country and the continent name when the query returns a response
   const countryContinentElement = await waitForElement(() => getByText(`America`))
   expect(countryNameElement).toBeDefined();
   expect(countryContinentElement).toBeDefined();
+})
+
+it('renders the country and the continent name when the query returns a response with jest', async () => {
+  const wrapper = mount(
+    <MockedProvider mocks={countriesWithCubaQueryMock} addTypename={false}>
+      <App />
+    </MockedProvider>
+  );
+
+  await wait(0);
+
+  // we could not find the elements by text, we can only refer
+  // to them if we add a test id or leak implmentation details
+
+  expect(wrapper.find(CountryDetails)).toBeDefined();
 })
