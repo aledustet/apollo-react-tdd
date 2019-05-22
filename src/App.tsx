c5ea4507e1a61786eee89935b48788df08385677
Alejandro Dustet <aledustet@gmail.com>
2019-05-21 20:26:04 -0400

    Make first test pass

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { FunctionComponent } from 'react';
 import './App.css';
 
-const App: React.FC = () => {
+const App: FunctionComponent = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>Hello Apollo</h1>
   );
 }
 
